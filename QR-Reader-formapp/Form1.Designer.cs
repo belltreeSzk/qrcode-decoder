f77d71747177c8ff7be24f2749ca0f3ba3b19d1d
TAKASHI\Takashi <tajukado@gmail.com>
2020-09-13 22:31:04 +0900

    first commit

---
+++ QR-Reader-formapp/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace QR_Reader_formapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.キャプチャ = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(72, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            // 
+            // キャプチャ
+            // 
+            this.キャプチャ.Location = new System.Drawing.Point(320, 505);
+            this.キャプチャ.Name = "キャプチャ";
+            this.キャプチャ.Size = new System.Drawing.Size(124, 44);
+            this.キャプチャ.TabIndex = 1;
+            this.キャプチャ.Text = "キャプチャ";
+            this.キャプチャ.UseVisualStyleBackColor = true;
+            this.キャプチャ.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(553, 521);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(789, 561);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.キャプチャ);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button キャプチャ;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ QR-Reader-formapp/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+
+using Microsoft.Win32;
+using static System.Console;
+using OpenCvSharp.Extensions;
+using ZXing;
+
+namespace QR_Reader_formapp
+{
+    public partial class Form1 : Form
+    {
+        int WIDTH = 640;
+        int HEIGHT = 480;
+        Mat frame;
+        VideoCapture capture;
+        Bitmap bmp;
+        Graphics graphic;
+        ZXing.BarcodeReader reader;
+        string prevData;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //カメラ画像取得用のVideoCapture作成
+            capture = new VideoCapture(0); // 0がインカメ, 1以降がウェブカメラ
+            if (!capture.IsOpened())
+            {
+                capture = new VideoCapture(1); // 0がインカメ, 1以降がウェブカメラ
+            }
+            if (!capture.IsOpened())
+            {
+                MessageBox.Show("camera was not found!");
+                this.Close();
+                throw new Exception();
+            }
+            capture.FrameWidth = WIDTH;
+            capture.FrameHeight = HEIGHT;
+
+            reader = new ZXing.BarcodeReader();
+
+            //取得先のMat作成
+            frame = new Mat(HEIGHT, WIDTH, MatType.CV_8UC3);
+
+            //表示用のBitmap作成
+            bmp = new Bitmap(frame.Cols, frame.Rows, (int)frame.Step(), System.Drawing.Imaging.PixelFormat.Format24bppRgb, frame.Data);
+
+            //PictureBoxを出力サイズに合わせる
+            pictureBox1.Width = frame.Cols;
+            pictureBox1.Height = frame.Rows;
+
+            //描画用のGraphics作成
+            graphic = pictureBox1.CreateGraphics();
+
+            //画像取得スレッド開始
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //スレッドの終了を待機
+            backgroundWorker1.CancelAsync();
+            while (backgroundWorker1.IsBusy)
+                Application.DoEvents();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker bw = (BackgroundWorker)sender;
+
+            while (!backgroundWorker1.CancellationPending)
+            {
+                //画像取得
+                capture.Grab();
+                NativeMethods.videoio_VideoCapture_operatorRightShift_Mat(capture.CvPtr, frame.CvPtr);
+
+                bw.ReportProgress(0);
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            //描画
+            graphic.DrawImage(bmp, 0, 0, frame.Cols, frame.Rows);
+            scanQRcode();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string userProfilePath = Environment.GetEnvironmentVariable("UserProfile");
+            string filePath = userProfilePath + @"\Desktop\cap.bmp";
+            frame.SaveImage(filePath);
+           
+            using (Mat cap = new Mat(filePath))
+            {
+                //保存されたキャプチャ画像の出力
+                Cv2.ImShow("test1", frame);
+            }
+        }
+
+        private void scanQRcode() {
+            string userProfilePath = Environment.GetEnvironmentVariable("UserProfile");
+
+            if (reader == null) {
+                return;
+            }
+            if (frame == null) {
+                return;
+            }
+            if (frame.ToBitmap() == null)
+            {
+                return;
+            }
+            // QRコードの解析
+            //ZXingに渡すのはBitmap
+            try {
+                ZXing.Result result = reader.Decode(frame.ToBitmap());
+                var text = result == null ? DateTime.Now.ToString() : result.Text;
+                label2.Text = text;
+                if (result != null && prevData != result.Text)
+                {
+                    File.AppendAllText(userProfilePath + @"\Desktop\log.txt", text + Environment.NewLine);
+                    // パラメータを指定して実行
+                    System.Diagnostics.Process.Start("notepad.exe", userProfilePath + @"\Desktop\log.txt");
+                    prevData = text;
+                }
+               
+            } catch (Exception exception) {
+                throw exception;
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
